321bd9a7f18b6bedbccf3fff45772b1c8b988e29
Temporary SDA User 1161 <tmp-sda-1161@kth.se>
2018-02-19 10:04:40 +0100

    add

--- TestArray.java
+++ TestArray.java
@@ -1,6 +1,7 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.Scanner;
 import java.util.ListIterator;
+import java.util.Comparator;
 class Student
 {
   private String name;
@@ -20,10 +21,24 @@ class Student
     return age;
   }
 
+  public static Comparator<Student> StuNameComparator = new Comparator<Student>()
+  {
+	   public int compare(Student s1, Student s2) {
+	   String StudentName1 = s1.getName().toUpperCase();
+	   String StudentName2 = s2.getName().toUpperCase();
+
+	   //ascending order
+	   return StudentName1.compareTo(StudentName2);
+
+	   //descending order
+	   //return StudentName2.compareTo(StudentName1);
+    }
+  };
 }
 class StudentList
 {
   private ArrayList<Student> StudentListArray = new ArrayList<>();
+  Student[] st = new Student[10];
   private Scanner scanString;
   private String stringName;
   private int intAge;
@@ -41,26 +56,30 @@ class StudentList
     intAge = Integer.parseInt(s);
     Student obj = new Student(stringName,intAge);
     StudentListArray.add(obj);
+    it = StudentListArray.listIterator();
 
   }
   public void viewFirst()
   {
-    if (it == null){ it = StudentListArray.listIterator();
-      {
+    //if (it == null)
+    { it = StudentListArray.listIterator();
+
         if (it.hasNext())
         {
         Student st = it.next();
         System.out.println("Student Name "+st.getName());
         System.out.println("Student age " +st.getage());
+        //it.next();
         }
       }
   }
-    System.out.println("First Student Name "+StudentListArray.get(0).getName());
-    System.out.println("First Student age " +StudentListArray.get(0).getage());
-  }
+
   public void viewNext()
   {
-    if(it == null){System.out.println("pointing to null");}
+    if(it == null){
+      it = StudentListArray.listIterator();
+      System.out.println("pointing to null");
+    }
     if (it.hasNext())
     {
       System.out.println("inside hasNext "+ it.nextIndex());
@@ -83,13 +102,35 @@ class StudentList
   }
   public void viewPrev()
   {
-    System.out.println("First Student Name "+StudentListArray.get(0).getName());
-    System.out.println("First Student age " +StudentListArray.get(0).getage());
+    if(it == null){
+      it = StudentListArray.listIterator();
+      System.out.println("pointing to null");
+    }
+    if (it.hasPrevious())
+    {
+      Student st = it.previous();
+      System.out.println("Student Name "+st.getName());
+      System.out.println("Student age " +st.getage());
+    }
   }
   public void removeS()
   {
-
+    if(it == null){
+      System.out.println("pointing to null");
+      it = StudentListArray.listIterator();
+    }
+    if (it.hasNext())
+    {
+      it.remove();
+      System.out.println("removed");
+      it = StudentListArray.listIterator();
+    }
   }
+  public void sortStudent()
+  {
+    Collections.sort(StudentListArray,Student.StuNameComparator);
+  }
+
 }
 class TestArray
 {
@@ -104,6 +145,8 @@ class TestArray
     System.out.println("Press 4 to view previous");
     System.out.println("Press 5 to remove current");
     System.out.println("Press 6 to view all");
+    System.out.println("Press 7 to sort on name");
+
     System.out.println("Press N to exit");
     scanString = new Scanner(System.in);
     String userInput = scanString.next();
@@ -120,6 +163,7 @@ class TestArray
         case "4": obj.viewPrev();break;
         case "5": obj.removeS();break;
         case "6": obj.viewAll();break;
+        case "7": obj.sortStudent();break;
 
       }
       System.out.println("What next");
